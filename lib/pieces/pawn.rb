80e8af4eb011e83585d5ff0d7da8e8d832a7e275
Kevin Lenehan <klenehan123@gmail.com>
2020-12-09 10:26:32 +0000

    don't need first move attribute

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,18 +1,15 @@
 # pawns
 class Pawn < Piece
-  attr_accessor :code, :first_move
+  attr_accessor :code
 
   def initialize(colour, position)
     super(colour, position)
     @code = ChessConstants::PIECE_CODES.dig(:PAWN, colour.to_sym)
-    @first_move = true
   end
 
-  
   def all_moves(position = @position)
     moves = []
     moves[0] = [position[0] + 1, position[1]]
-    p @initial_position
     if @position == @initial_position
       moves[1] = [position[0] + 2, position[1]]
     end
