65c17f80405dc741fc645967d36bc7434460cc32
Kevin Lenehan <klenehan123@gmail.com>
2020-12-09 10:28:28 +0000

    diagonal moves working but can jump pieces, need to fix

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -10,14 +10,75 @@ class Bishop < Piece
 
   def all_moves(position = @position)
     moves = []
-    moves[0] = [position[0] + 2, position[1] + 1]
-    moves[1] = [position[0] + 2, position[1] - 1]
-    moves[2] = [position[0] - 2, position[1] + 1]
-    moves[3] = [position[0] - 2, position[1] - 1]
-    moves[4] = [position[0] + 1, position[1] + 2]
-    moves[5] = [position[0] - 1, position[1] + 2]
-    moves[6] = [position[0] + 1, position[1] - 2]
-    moves[7] = [position[0] - 1, position[1] - 2]
+
+    p moves
+    moves.concat(diag_up_right) if diag_up_right.length.positive?
+    p moves
+    moves.concat(diag_up_left) if diag_up_left.length.positive?
+    p moves
+    moves.concat(diag_down_right) if diag_down_right.length.positive?
+    p moves
+    moves.concat(diag_down_left) if diag_down_left.length.positive?
+    p moves
     moves
   end
+
+  def diag_up_right
+    diag_up_right_moves = []
+    top = 7 - position[0]
+    right = 7 - position[1]
+    i = 0
+    while top.positive? && right.positive?
+      diag_up_right_moves[i] = [position[0] + 1 + i, position[1] + 1 + i]
+      i += 1
+      top -= 1
+      right -= 1
+    end
+    diag_up_right_moves
+  end
+
+  def diag_up_left
+    diag_up_left_moves = []
+    top = 7 - position[0]
+    bottom = position[0]
+    right = 7 - position[1]
+    left = position[1]
+    i = 0
+    while top.positive? && left.positive?
+      diag_up_left_moves[i] = [position[0] + 1 + i, position[1] - 1 - i]
+      i += 1
+      top -= 1
+      left -= 1
+    end
+    diag_up_left_moves
+  end
+
+  def diag_down_right
+    diag_down_right_moves = []
+    bottom = position[0]
+    right = 7 - position[1]
+    i = 0
+    while bottom.positive? && right.positive?
+      diag_down_right_moves[i] = [position[0] - 1 - i, position[1] + 1 + i]
+      i += 1
+      bottom -= 1
+      right -= 1
+    end
+    diag_down_right_moves
+  end
+
+  def diag_down_left
+    diag_down_left_moves = []
+    bottom = position[0]
+    left = position[1]
+    i = 0
+    while bottom.positive? && left.positive?
+      diag_down_left_moves[i] = [position[0] - 1 - i, position[1] - 1 - i]
+      i += 1
+      bottom -= 1
+      left -= 1
+    end
+    diag_down_left_moves
+  end
+
 end
