6c3bc028f080958d6375eefb65fa252a50060a0a
Kevin Lenehan <klenehan123@gmail.com>
2020-12-10 17:50:55 +0000

    bishop inherits from queen

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,5 +1,5 @@
 # Bishop
-class Bishop < Piece
+class Bishop < Queen
   attr_accessor :code
 
   def initialize(colour, position)
@@ -7,7 +7,7 @@ class Bishop < Piece
     @code = ChessConstants::PIECE_CODES.dig(:BISHOP, colour.to_sym)
   end
 
-  def all_moves(_position = @position)
+  def all_moves(position = @position)
     moves = []
     moves.concat([diag_moves(position, "up", "right")])
     moves.concat([diag_moves(position, "up", "left")])
@@ -15,25 +15,5 @@ class Bishop < Piece
     moves.concat([diag_moves(position, "down", "left")])
     moves
   end
-
-  def diag_moves(position, vert, horiz)
-    diag_moves = []
-
-    v_index = vert == "up" ? 7 - position[0] : position[0]
-    h_index = horiz == "right" ? 7 - position[1] : position[1]
-
-    i = 0
-    while v_index.positive? && h_index.positive?
-      rank = vert == "up" ? position[0] + 1 + i : position[0] - 1 - i
-      file = horiz == "right" ? position[1] + 1 + i : position[1] - 1 - i
-      diag_moves[i] = [rank, file]
-
-      i += 1
-      v_index -= 1
-      h_index -= 1
-    end
-    diag_moves
-  end
-
 
 end
