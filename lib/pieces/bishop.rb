ecd8b2ed9c5bfddadf6d8f38377561d3ac42eb7a
Kevin Lenehan <klenehan123@gmail.com>
2020-12-10 17:36:00 +0000

    new compacted diag working

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -9,14 +9,10 @@ class Bishop < Piece
 
   def all_moves(_position = @position)
     moves = []
-
-#    moves << diag_up_right if diag_up_right.length.positive?
-    up_right_moves = diag_moves(position, "up", "right")
-    moves << up_right_moves if up_right_moves.length.positive?
-
-    moves << diag_up_left if diag_up_left.length.positive?
-    moves << diag_down_right if diag_down_right.length.positive?
-    moves << diag_down_left if diag_down_left.length.positive?
+    moves.concat([diag_moves(position, "up", "right")])
+    moves.concat([diag_moves(position, "up", "left")])
+    moves.concat([diag_moves(position, "down", "right")])
+    moves.concat([diag_moves(position, "down", "left")])
     moves
   end
 
@@ -39,59 +35,5 @@ class Bishop < Piece
     diag_moves
   end
 
-  def diag_up_right
-    diag_up_right_moves = []
-    top = 7 - position[0]
-    right = 7 - position[1]
-    i = 0
-    while top.positive? && right.positive?
-      diag_up_right_moves[i] = [position[0] + 1 + i, position[1] + 1 + i]
-      i += 1
-      top -= 1
-      right -= 1
-    end
-    diag_up_right_moves
-  end
 
-  def diag_up_left
-    diag_up_left_moves = []
-    top = 7 - position[0]
-    left = position[1]
-    i = 0
-    while top.positive? && left.positive?
-      diag_up_left_moves[i] = [position[0] + 1 + i, position[1] - 1 - i]
-      i += 1
-      top -= 1
-      left -= 1
-    end
-    diag_up_left_moves
-  end
-
-  def diag_down_right
-    diag_down_right_moves = []
-    bottom = position[0]
-    right = 7 - position[1]
-    i = 0
-    while bottom.positive? && right.positive?
-      diag_down_right_moves[i] = [position[0] - 1 - i, position[1] + 1 + i]
-      i += 1
-      bottom -= 1
-      right -= 1
-    end
-    diag_down_right_moves
-  end
-
-  def diag_down_left
-    diag_down_left_moves = []
-    bottom = position[0]
-    left = position[1]
-    i = 0
-    while bottom.positive? && left.positive?
-      diag_down_left_moves[i] = [position[0] - 1 - i, position[1] - 1 - i]
-      i += 1
-      bottom -= 1
-      left -= 1
-    end
-    diag_down_left_moves
-  end
 end
