37e19e8c0bfbcfae997e641c6259d32252df6909
Kevin Lenehan <klenehan123@gmail.com>
2020-12-13 12:07:00 +0000

    pawn taking working

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -15,7 +15,14 @@ class Pawn < Piece
     moves[0] = [[position[0] + one_square, position[1]]]
     if @position == @initial_position
       moves[1] = [[position[0] + two_squares, position[1]]]
+    else
+      moves[1] = []
     end
+
+    # attack moves
+    moves[2] = [[position[0] + one_square, position[1] + 1]]
+    moves[3] = [[position[0] + one_square, position[1] - 1]]
+
     moves
   end
 end
